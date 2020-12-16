dbd5907c45e4327be097f5f6047ba410d3beaa11
TaihouDaisuki <LoveTaihouForever@hotmail.com>
2020-12-16 14:01:14 +0800

    update

--- Physarum/CPPA.cpp
+++ Physarum/CPPA.cpp
@@ -13,7 +13,7 @@ using namespace std;
 const int Maxn = 110;
 const int Maxm = 10010;
 const int inf = 999999999 + 413;
-const int Maxc = 10000;
+const int Maxc = 1000000;
 const double eps = 1e-6;
 
 int N, M, I0;
@@ -25,9 +25,9 @@ double D[Maxn][Maxn];
 double p[Maxn]; // pressure
 // var for solving p
 double tI[Maxn];
-double para[Maxn][Maxn];
+double para[Maxn][Maxn], _para[Maxn][Maxn];
 
-void Gauss(const int n, double(*MatrixA)[Maxn], double* MatrixC, double* MatrixB)
+void Gauss(const int n, double(*MatrixA)[Maxn], double* MatrixB, double* MatrixC)
 {
 	const double epx = 1e-6;
 
@@ -35,10 +35,12 @@ void Gauss(const int n, double(*MatrixA)[Maxn], double* MatrixC, double* MatrixB
 	{
 		int maxi = i;
 		for (int j = i + 1; j <= n; j++)
-			if (fabs(MatrixA[j][i]) > fabs(MatrixA[maxi][i])) maxi = j;
+			if (fabs(MatrixA[j][i]) > fabs(MatrixA[maxi][i])) 
+				maxi = j;
 		if (maxi != i)
 		{
-			for (int j = i; j <= n; j++) swap(MatrixA[i][j], MatrixA[maxi][j]);
+			for (int j = i; j <= n; j++) 
+				swap(MatrixA[i][j], MatrixA[maxi][j]);
 			swap(MatrixC[i], MatrixC[maxi]);
 		}
 
@@ -46,7 +48,8 @@ void Gauss(const int n, double(*MatrixA)[Maxn], double* MatrixC, double* MatrixB
 		{
 			if (fabs(MatrixA[j][i]) < 1e-6) continue;
 			double tmp = MatrixA[i][i] / MatrixA[j][i];
-			for (int k = i; k <= n; k++) MatrixA[j][k] = MatrixA[j][k] * tmp - MatrixA[i][k];
+			for (int k = i; k <= n; k++) 
+				MatrixA[j][k] = MatrixA[j][k] * tmp - MatrixA[i][k];
 			MatrixC[j] = MatrixC[j] * tmp - MatrixC[i];
 		}
 	}
@@ -54,7 +57,8 @@ void Gauss(const int n, double(*MatrixA)[Maxn], double* MatrixC, double* MatrixB
 	for (int i = n; i; i--)
 	{
 		MatrixB[i] = MatrixC[i] / MatrixA[i][i];
-		for (int j = 1; j < i; j++) MatrixC[j] -= MatrixB[i] * MatrixA[j][i];
+		for (int j = 1; j < i; j++) 
+			MatrixC[j] -= MatrixB[i] * MatrixA[j][i];
 	}
 }
 
@@ -94,7 +98,22 @@ int CPPA(const double k = 0.7) // k is the parameter of the capacity
 			else
 				tI[j] = 0;
 		}
-		Gauss(N, para, p, tI);
+		
+		// set p[S] = 0, remove the para
+		for (int i = 1; i <= N; ++i)
+		{
+			int _j = 0;
+			for (int j = 1; j <= N; ++j)
+			{
+				if (j == S)
+					continue;
+				_para[i][++_j] = para[i][j];
+			}
+		}
+		Gauss(N - 1, _para, p, tI);
+		for (int i = N - 1; i >= S; --i)
+			p[i + 1] = p[i];
+		p[S] = 0;
 
 		/* Step two, caculate the value of Q[i][j], using the former p[i] */
 		for (int i = 1; i <= N; ++i)
@@ -112,11 +131,23 @@ int CPPA(const double k = 0.7) // k is the parameter of the capacity
 						D[i][j] = 1.0 * C[i][j] * L[i][j] / fabs(p[i] - p[j]);
 		++count;
 	}
+
+	for (int i = 1; i <= N; ++i)
+	{
+		for (int j = 1; j <= N; ++j)
+		{
+			printf("Q[%d][%d] = %.4lf, ", i, j, Q[i][j]);
+		}
+		puts("");
+	}
+	
+	return 1; //******
 }
 void work()
 {
-	int ans = CPPA();
-	printf("%d\n", ans);
+	CPPA();
+	// int ans = CPPA();
+	// printf("%d\n", ans);
 }
 
 inline int get()
@@ -128,13 +159,14 @@ inline int get()
 }
 void init()
 {
-	int a, b, w;
+	int a, b;
 	N = get(); M = get(); S = 1; T = N;
 	I0 = get();
 	for (int i = 1; i <= M; ++i)
 	{
 		a = get(); b = get();
-		L[a][b] = get();  C[a][b] = get();
+		L[a][b] = L[b][a] = get();  
+		C[a][b] = C[b][a] = get();
 	}
 }
 
